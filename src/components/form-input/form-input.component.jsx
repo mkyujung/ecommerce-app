5e19a8ed3c51bff3c113aacea88bf1fcd316d3da
Min Kyu Jung <mkyujung@gmail.com>
2019-11-10 10:16:18 +1300

    implemented firebase utils, included ability to store authenticated users in database

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
+
 import './form-input.styles.scss';
 
-const FormInput = ({ handleChange, label, value, ... otherProps }) => (
-    <div className="group">
-        <input className='form-input' onChange={handleChange} {...otherProps} />
-    {
-        label ? 
-        (<label 
-            //check whether anything has been typed into form - if so apply 'shrink' class
-            className={`${
-                value.length} ? 'shrink' : ''
-                } form-input-label `}
-        >
-            {label}
-        </label>
-        ) : null
-    }
-    
-    </div>
-)
+const FormInput = ({ handleChange, label, ...otherProps }) => (
+  <div className='group'>
+    <input className='form-input' onChange={handleChange} {...otherProps} />
+    {label ? (
+      <label
+        className={`${
+          otherProps.value.length ? 'shrink' : ''
+        } form-input-label`}
+      >
+        {label}
+      </label>
+    ) : null}
+  </div>
+);
 
 export default FormInput;
